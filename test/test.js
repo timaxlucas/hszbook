499b9d8d17ebc0ec3a91e575a4b22400b2b61e2a
Maximilian Lucas <dev@maximilian-lucas.de>
2019-05-23 17:58:19 +0200

    test ci

--- test/test.js
+++ test/test.js
@@ -3,5 +3,6 @@ const { getData } = require('../');
 
 test('getData', async() => {
   const res = await getData('https://buchung.hsz.rwth-aachen.de/angebote/Sommersemester_2019/_Volleyball_Spielbetrieb.html');
+  console.log(res);
   expect(typeof res).toBe('object');
 });
