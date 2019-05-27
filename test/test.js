8d702ade104187452545cfde2c3c16f8ff322d27
Maximilian Lucas <dev@maximilian-lucas.de>
2019-05-27 23:46:20 +0200

    🧪 updated tests

--- test/test.js
+++ test/test.js
@@ -1,27 +1,70 @@
 const { getData, registerForCourse } = require('../');
 
-
+/*
 test('getData', async() => {
+  expect.assertions(1);
+
   const res = await getData('https://buchung.hsz.rwth-aachen.de/angebote/Sommersemester_2019/_Volleyball_Spielbetrieb.html');
   expect(Array.isArray(res)).toBe(true);
+});*/
+
+
+describe('registerForCourse', () => {
+
+  test('successful registration', async() => {
+    expect.assertions(1);
+
+    const data = {
+      firstname: 'John',
+      surname: 'Schnee',
+      gender: 'male',
+      street: 'moo street 13',
+      city: '12345 city',
+      matrnr: '123456',
+      email: 'asd.fgh@rwth-aachen.de',
+      phone: '0123456789',
+      iban: 'DE12 3456 7891 0123 4567 89'
+    };
+    const url = 'https://buchung.hsz.rwth-aachen.de/angebote/Sommersemester_2019/_Basketball_Spielbetrieb.html';
+    const kid = '13131823';
+
+    const { success, message } = await registerForCourse(url, kid, data, true, {debug: true});
+    console.log(success, message);
+    expect(success).toBe(true);
+  }, 60 * 1000);
+
+  test('data fields missing', async() => {
+    expect.assertions(1);
+
+    const data = {};
+    const url = 'https://buchung.hsz.rwth-aachen.de/angebote/Sommersemester_2019/_Basketball_Spielbetrieb.html';
+    const kid = '13131823';
+    try {
+      await registerForCourse(url, kid, data, true, {debug: false});
+    } catch (e) {
+      expect(e).toMatch(/parameter data is missing/);
+    }
+  });
+
+  test('invalid data format', async() => {
+    expect.assertions(2);
+
+    const data = {
+      firstname: 'John',
+      surname: 'Schnee',
+      gender: 'male',
+      street: 'moo street 13',
+      city: 'city',
+      matrnr: '123456',
+      email: 'asd.fgh@rwth-aachen.de',
+      phone: '0123456789',
+      iban: 'DE12 3456 7891 0123 4567 89'
+    };
+    const url = 'https://buchung.hsz.rwth-aachen.de/angebote/Sommersemester_2019/_Basketball_Spielbetrieb.html';
+    const kid = '13131823';
+
+    const { success, message } = await registerForCourse(url, kid, data, true, {debug: false});
+    expect(success).toBe(false);
+    expect(message).toMatch(/data validation failed/);
+  }, 40 * 1000);
 });
-
-
-test('registerForCourse', async() => {
-  const data = {
-    firstname: 'John',
-    surname: 'Schnee',
-    gender: 'male',
-    street: 'moo street 13',
-    city: '12345 city',
-    matrnr: '123456',
-    email: 'asd.fgh@rwth-aachen.de',
-    phone: '0123456789',
-    iban: 'DE12 3456 7891 0123 4567 89'
-  };
-  const url = 'https://buchung.hsz.rwth-aachen.de/angebote/Sommersemester_2019/_Basketball_Spielbetrieb.html';
-  const kid = '13131823';
-
-  const { success } = await registerForCourse(url, kid, data, true);
-  expect(success).toBe(true);
-}, 60 * 1000);
